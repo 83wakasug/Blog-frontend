1bc6e417ed7e36afdba3115ee6dac74cc66f5afb
Wakana <pre.kuro.05@gmail.com>
2024-09-11 22:08:13 +0200

    add css and js file for blog

---
+++ blog.js
@@ -0,0 +1,11 @@
+
+
+    // submit時にイベント実行をする関数
+     function showText(event) {
+        // 再読み込み防止
+        event.preventDefault();
+        // 入力フォームの内容を取得
+        let inputForm = document.getElementById('form').content.value;
+		// 入力内容を画面に出力
+        document.getElementById('output').textContent = `${inputForm}`;
+    }
